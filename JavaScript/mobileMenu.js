749c4c5381e0dc7b1af79e295ff88c5864946055
eserengo <eserengo@gmail.com>
2022-09-05 16:30:05 -0300

    Added changes to html and css files

--- JavaScript/mobileMenu.js
+++ JavaScript/mobileMenu.js
@@ -1,7 +1,7 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileBtn = document.getElementById('mobile-menu-btn');
 const closeBtn = document.getElementById('cancel-btn');
-const menu = document.querySelector('.menu')
+const menu = document.querySelector('.menu');
 
 mobileBtn.onclick = () => {
   mobileMenu.style.display = 'block';
@@ -12,5 +12,5 @@ closeBtn.onclick = () => {
 };
 
 menu.addEventListener('click', () => {
-  mobileMenu.style.display = 'none'
-})
+  mobileMenu.style.display = 'none';
+});
