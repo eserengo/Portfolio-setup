2b82c18f77992f8510994a02bde86405c7a0bc69
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-09-05 22:26:15 +0500

    Update styling and javascript

--- JavaScript/mobileMenu.js
+++ JavaScript/mobileMenu.js
@@ -1,6 +1,7 @@
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileBtn = document.getElementById('mobile-menu-btn');
 const closeBtn = document.getElementById('cancel-btn');
+const menu = document.querySelector('.menu')
 
 mobileBtn.onclick = () => {
   mobileMenu.style.display = 'block';
@@ -9,3 +10,7 @@ mobileBtn.onclick = () => {
 closeBtn.onclick = () => {
   mobileMenu.style.display = 'none';
 };
+
+menu.addEventListener('click', () => {
+  mobileMenu.style.display = 'none'
+})
