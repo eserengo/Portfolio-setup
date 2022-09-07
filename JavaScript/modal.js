64b0b29906087e5daf54db508d58944140d167e0
eserengo <eserengo@gmail.com>
2022-09-07 11:56:51 -0300

    Added last changes to modal.js index.html and style.css

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,66 +1,134 @@
 /* eslint-disable linebreak-style */
+// Modal Script
+window.onload = () => {
+  const projectsData = [
+    {
+      title: 'Tonic',
+      subtitle: 'CANOPY',
+      techStack: 'Back End Dev',
+      year: 2015,
+      src: './images/Snapshoot-portfolio-1.png',
+      highlight: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      lang: ['html', 'css', 'javascript'],
+      live: 'https://eserengo.github.io/Portfolio-setup/',
+      source: 'https://github.com/eserengo/Portfolio-setup',
+    },
+    {
+      title: 'Multi Post Stories',
+      subtitle: 'CANOPY',
+      techStack: 'Back End Dev',
+      year: 2015,
+      src: './images/Snapshoot-portfolio-2.png',
+      highlight: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      lang: ['html', 'css', 'javascript'],
+      live: 'https://eserengo.github.io/Portfolio-setup/',
+      source: 'https://github.com/eserengo/Portfolio-setup',
+    },
+    {
+      title: 'Tonic',
+      subtitle: 'CANOPY',
+      techStack: 'Back End Dev',
+      year: 2015,
+      src: './images/Snapshoot-portfolio-3.png',
+      highlight: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      lang: ['html', 'css', 'javascript'],
+      live: 'https://eserengo.github.io/Portfolio-setup/',
+      source: 'https://github.com/eserengo/Portfolio-setup',
+    },
+    {
+      title: 'Multi Post Stories',
+      subtitle: 'CANOPY',
+      techStack: 'Back End Dev',
+      year: 2015,
+      src: './images/Snapshoot-portfolio-4.png',
+      highlight: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+      lang: ['html', 'css', 'javascript'],
+      live: 'https://eserengo.github.io/Portfolio-setup/',
+      source: 'https://github.com/eserengo/Portfolio-setup',
+    },
+  ];
 
-const cards = document.querySelectorAll('.work');
-const cardProp = [];
+  const projectCardString = projectsData.map((project) => `
+    <div class="card">
+      <div class="inner-card">
+        <div class="card-img card-1">
+          <img
+            src="${project.src}"
+            alt="Project Snapshoot"
+          />
+        </div>
+        <div class="desc">
+          <h3>${project.title}</h3>
+          <h4>${project.subtitle} <span>${project.techStack}</span> <span>${project.year}</span></h4>
+          <p>${project.highlight}</p>
+          <ul>
+            ${project.lang.map((tech) => `<li>${tech}</li>`).join('')}
+          </ul>
+          <button type="button" class="project-btn button">
+            See Project
+          </button>
+        </div>
+      </div>
+    </div>
+  `);
 
-function CardObject(
-  title,
-  timeline,
-  picture,
-  description,
-  technology,
-  liveURL,
-  sourceURL,
-) {
-  this.title = title;
-  this.timeline = timeline;
-  this.picture = picture;
-  this.description = description;
-  this.technology = technology;
-  this.liveURL = liveURL;
-  this.sourceURL = sourceURL;
-}
+  const popupContainer = document.querySelector('.project-popup-container');
+  const recentWork = document.querySelector('.recent-work');
+  const parser = new DOMParser();
 
-for (let index = 0; index < cards.length; index += 1) {
-  const title = cards[index].querySelector('.work-header').innerText;
-  const timeline = cards[index].querySelector('.workframe').innerHTML;
-  const picture = cards[index].querySelector('.img-container').src;
-  const description = cards[index].querySelector('.work-primary-text').innerText;
-  const technology = cards[index].querySelector('.worktag').innerHTML;
-  const liveURL = 'https://eserengo.github.io/Portfolio-setup/';
-  const sourceURL = 'https://github.com/eserengo/Portfolio-setup';
+  projectCardString.forEach((projectString, index) => {
+    const projectElement = parser.parseFromString(projectString, 'text/html').body.firstChild;
 
-  cardProp.push(
-    new CardObject(
-      title,
-      timeline,
-      picture,
-      description,
-      technology,
-      liveURL,
-      sourceURL,
-    ),
-  );
-}
+    const mobilePopup = `
+      <div class="project-popup-hidden ">
+        <div class="project-background"></div>
+          <div class="project-popup">
+            <div class="project-header">
+              <i class="fa-solid fa-xmark project-close"></i>
+              <h2>${projectsData[index].title}</h2>
+              <p>${projectsData[index].subtitle} <span>${projectsData[index].techStack}</span> <span>${projectsData[index].year}</span></p>
+            </div>
+  
+            <div class="project-img">
+            <img
+            src="${projectsData[index].src}"
+            alt="Project Snapshoot"
+            /> 
+            </div>
+  
+            <div class="project-desc">
+              <p>${projectsData[index].description}</p>
+              <ul>
+                ${projectsData[index].lang.map((tech) => `<li>${tech}</li>`).join('')}
+              </ul>
+            </div>
+  
+            <div class="project-links">
+              <a href="${projectsData[index].live}">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+              <a href="${projectsData[index].source}">See Source <i class="fa-brands fa-github"></i></a>
+            </div>
+        </div>
+      </div>
+    `;
 
-const modalButton = document.querySelectorAll('.see');
-const modal = document.querySelector('.modal-container');
-const closeModal = document.getElementById('close-modal');
-for (let index = 0; index < modalButton.length; index += 1) {
-  modalButton[index].setAttribute('data-index', index);
-  modalButton[index].addEventListener('click', (e) => {
-    const pos = e.target.getAttribute('data-index');
-    modal.querySelector('.modal-title').innerHTML = cardProp[pos].title;
-    modal.querySelector('.timeline').innerHTML = cardProp[pos].timeline;
-    modal.querySelector('.img-container').src = cardProp[pos].picture;
-    modal.querySelector('.modal-desc').innerHTML = cardProp[pos].description;
-    modal.querySelector('.worktag').innerHTML = cardProp[pos].technology;
-    modal.querySelector('.live').href = cardProp[pos].liveURL;
-    modal.querySelector('.source').href = cardProp[pos].sourceURL;
-    modal.style.display = 'block';
+    const mobilePopupElement = parser.parseFromString(mobilePopup, 'text/html').body.firstChild;
+
+    const projectBtn = projectElement.querySelector('.project-btn');
+    const closeBtn = mobilePopupElement.querySelector('.project-close');
+
+    projectBtn.addEventListener('click', () => {
+      mobilePopupElement.classList.toggle('project-popup-hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      mobilePopupElement.classList.toggle('project-popup-hidden');
+    });
+
+    recentWork.append(projectElement);
+    popupContainer.append(mobilePopupElement);
   });
-}
-
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
+};
